b41eb2e1f6ac89aecf41a593fe9c9c4e0e52cd6d
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-03 19:55:24 +0000

    Added server code to post to MongoDB Database

--- bd-brewery-app/Server/app.py
+++ bd-brewery-app/Server/app.py
@@ -2,12 +2,13 @@ from flask import Flask
 
 # Routes
 from flask_cors import CORS
-from routes import indexRoute
+from routes import indexRoute, createBCSRoute
 app = Flask(__name__)
 CORS(app)
 
 # Register Blueprints
 app.register_blueprint(indexRoute)
+app.register_blueprint(createBCSRoute)
 
 
 
--- bd-brewery-app/Server/routes.py
+++ bd-brewery-app/Server/routes.py
@@ -15,8 +15,9 @@ class JSONEncoder(json.JSONEncoder):
 
 # Configure MongoDB with mlab connection
 
-# Set up connection with user and password of database
-connection = "mongodb://bdbrewery:bdbrewery1@ds241968.mlab.com:41968/bd_brewery"
+# Set up connection with user and password of database 
+#- retrywrites needs to be set to default as not supported in enviornment - https://stackoverflow.com/questions/57836252/how-to-fix-retrywrites-in-mongo
+connection = "mongodb://bdbrewery:bdbrewery1@ds241968.mlab.com:41968/bd_brewery?retryWrites=false"
 # get a connecion with the database in mlab
 client = MongoClient(connection)
 # Connect to database by name
@@ -27,8 +28,27 @@ collection = db["brewcontrolsheet"]
 
 #Blueprints
 indexRoute = Blueprint("index", __name__)
+createBCSRoute = Blueprint("createBCS", __name__)
 
 #routes 
 @indexRoute.route("/api/brewcontrolsheet")
 def index():
     return jsonify(data="Test")
+
+@createBCSRoute.route("/api/createbrewcontrolsheet", methods=["POST"])
+def createBCS():
+    print(request.json, flush=True)
+
+    brewNo = request.json.get("brewNo")
+    beer = request.json.get("beer")
+
+    print("Brew Number:" +brewNo + " Beer:" + beer)
+
+    brewcontrolsheet = {
+        "brewNo": brewNo,
+        "beer": beer
+    }
+
+    collection.insert_one(brewcontrolsheet)
+
+    return jsonify(data="Brew Control sheet created successfully")
