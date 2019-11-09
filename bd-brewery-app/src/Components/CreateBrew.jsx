6fb58f9b1e608f503e6cf7344418eb3917ee822a
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-09 12:13:09 +0000

    Fixed form format so that it splits in two
    also improved css to make it look better

--- bd-brewery-app/src/Components/CreateBrew.jsx
+++ bd-brewery-app/src/Components/CreateBrew.jsx
@@ -7,12 +7,14 @@ const url = "http://127.0.0.1:5000/"
 
 // Set some styling for div
 const divStyle = {
-    width: '500px',
+    width: '48%',
     border: '5px',
     background: '#f2f2f2',
     padding: '20px',
     margin: '10px'
   };
+
+
 
   // react arrow function component to create a brew
   const CreateBrew = () => {
@@ -81,54 +83,53 @@ const divStyle = {
     return(
         // React Fragment is a way of sending back multiple elements - https://reactjs.org/docs/fragments.html
         <React.Fragment> 
-            <div style={divStyle}>
-                //form which calls creatre function when submitted
                 <form onSubmit={create}>
-                    <label>Brew No.</label>
-                    <input type="text" name="BrewNo" placeholder="Enter Brew Number" onChange= {event => setBrewNo(event.target.value)}/>
+                    <div style={divStyle} className="float-left">
+                        <label>Brew No.</label>
+                        <input type="text" name="BrewNo" placeholder="Enter Brew Number" onChange= {event => setBrewNo(event.target.value)}/>
 
-                    <label>Beer</label>
-                    <input type="text" name="beer" placeholder="Enter Beer" onChange={event => setBeer(event.target.value)}/>
+                        <label>Beer</label>
+                        <input type="text" name="beer" placeholder="Enter Beer" onChange={event => setBeer(event.target.value)}/>
 
-                    <label>Batch No</label>
-                    <input type="text" name="batchNo" placeholder="Enter Batch Number" onChange={event => setBatchNo(event.target.value)}/>
+                        <label>Batch No</label>
+                        <input type="text" name="batchNo" placeholder="Enter Batch Number" onChange={event => setBatchNo(event.target.value)}/>
 
-                    <label>Brew Date</label>
-                    <input type="text" name="brewDate" placeholder="Enter Brew Date" onChange={event => setBrewDate(event.target.value)}/>
+                        <label>Brew Date</label>
+                        <input type="text" name="brewDate" placeholder="Enter Brew Date" onChange={event => setBrewDate(event.target.value)}/>
 
-                    <label>OG (Original Gravity)</label>
-                    <input type="text" name="og" placeholder="Enter OG" onChange={event => setOG(event.target.value)}/>
+                        <label>OG (Original Gravity)</label>
+                        <input type="text" name="og" placeholder="Enter OG" onChange={event => setOG(event.target.value)}/>
 
-                    <label>PG (Present Gravity)</label>
-                    <input type="text" name="pg" placeholder="Enter PG" onChange={event => setPG(event.target.value)}/>
+                        <label>PG (Present Gravity)</label>
+                        <input type="text" name="pg" placeholder="Enter PG" onChange={event => setPG(event.target.value)}/>
 
-                    <label>ABV</label>
-                    <input type="text" name="abv" placeholder="Enter ABV" onChange={event => setABV(event.target.value)}/>
+                        <label>ABV</label>
+                        <input type="text" name="abv" placeholder="Enter ABV" onChange={event => setABV(event.target.value)}/>
+                    </div>
+                    <div className="float-right" style={divStyle}>
+                        <label>Post Conditioning Date</label>
+                        <input type="text" name="postConditionDate" placeholder="Enter Post Conditioning Date" onChange={event => setPCD(event.target.value)}/>
 
-                    <label>Post Conditioning Date</label>
-                    <input type="text" name="postConditionDate" placeholder="Enter Post Conditioning Date" onChange={event => setPCD(event.target.value)}/>
+                        <label>Post Conditioning Volume</label>
+                        <input type="text" name="postConditionVol" placeholder="Enter Post Conditioning Volume" onChange={event => setPCV(event.target.value)}/>
 
-                    <label>Post Conditioning Volume</label>
-                    <input type="text" name="postConditionVol" placeholder="Enter Post Conditioning Volume" onChange={event => setPCV(event.target.value)}/>
+                        <label>Keg No</label>
+                        <input type="text" name="kegNo" placeholder="Enter Keg Number" onChange={event => setKegNo(event.target.value)}/>
 
-                    <label>Keg No</label>
-                    <input type="text" name="kegNo" placeholder="Enter Keg Number" onChange={event => setKegNo(event.target.value)}/>
+                        <label>Bottle Number (500ml)</label>
+                        <input type="text" name="bottleNo500" placeholder="Enter Bottle Number (500ml)" onChange={event => setBottleNo500(event.target.value)}/>
 
-                    <label>Bottle Number (500ml)</label>
-                    <input type="text" name="bottleNo500" placeholder="Enter Bottle Number (500ml)" onChange={event => setBottleNo500(event.target.value)}/>
+                        <label>Bottle Number (330ml)</label>
+                        <input type="text" name="bottleNo330" placeholder="Enter Bottle Number (330ml)" onChange={event => setBottleNo330(event.target.value)}/>
 
-                    <label>Bottle Number (330ml)</label>
-                    <input type="text" name="bottleNo330" placeholder="Enter Bottle Number (330ml)" onChange={event => setBottleNo330(event.target.value)}/>
+                        <label>Duty</label>
+                        <input type="text" name="duty" placeholder="Enter Duty" onChange={event => setDuty(event.target.value)}/>
 
-                    <label>Duty</label>
-                    <input type="text" name="duty" placeholder="Enter Duty" onChange={event => setDuty(event.target.value)}/>
-
-                    <label>Status</label>
-                    <input type="text" name="status" placeholder="Enter Status" onChange={event => setStatus(event.target.value)}/>
-                
-                    <input type="submit" value="Submit"/>
+                        <label>Status</label>
+                        <input type="text" name="status" placeholder="Enter Status" onChange={event => setStatus(event.target.value)}/>
+                    </div>
+                    <input type="submit" value="Create Brew"/>
                 </form>
-            </div>
         </React.Fragment>)
 }
     
