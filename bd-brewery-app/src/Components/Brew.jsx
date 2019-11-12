8c063e7e36bd5a311078592bc73962063d388586
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-12 23:15:22 +0000

    Displayed the data recieved back from the server in React

--- bd-brewery-app/src/Components/Brew.jsx
+++ bd-brewery-app/src/Components/Brew.jsx
@@ -22,12 +22,35 @@ const Brew = () => {
     getBrews();
   }, [])
 
+  let brewsArray;
+  if(brews.length > 0){
+      brewsArray = <div>
+        {brews.map(brew => {
+          return(
+            <div key={brew._id}>
+              <Link to={"brew/"+brew._id}>
+                <p>Brew Number: {brew.brewNo} / Beer:{brew.beer}</p>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+  }else{
+    brewsArray = 
+    <div>
+      <p>No Brews exist in the database, please create a brew</p>
+    </div>
+  }
+
 return(
     <React.Fragment> 
       <div>
           <h1>Create a Brew</h1>
           <Link to="/createbrew">create a control sheet</Link><br/>
-          <Link to="/" >Back</Link>
+          <Link to="/" >Back</Link><br/><br/>
+
+          {brewsArray}
+
       </div>
     </React.Fragment>)
   }
