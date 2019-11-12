c5ae8792fc218117632288a4b4a3cc38b4ea39ce
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-12 22:02:25 +0000

    Added react code to retrieve some sample data from server

--- bd-brewery-app/src/Components/Brew.jsx
+++ bd-brewery-app/src/Components/Brew.jsx
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 
-class Brew extends React.Component {
-    render() {
-      return <div>
+//set the url to receive the data from
+const url = "http://127.0.0.1:5000/"
+
+const Brew = () => {
+
+  const [brews, setbrews] = useState([]);
+  const getBrews = () => {
+    fetch(url+"api/brew").then(res =>{
+      return res.json();
+    }).then(brews => {
+      console.log(brews);
+      setbrews(brews.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    getBrews();
+  }, [])
+
+return(
+    <React.Fragment> 
+      <div>
           <h1>Create a Brew</h1>
           <Link to="/createbrew">create a control sheet</Link><br/>
           <Link to="/" >Back</Link>
-      </div>;
-    }
+      </div>
+    </React.Fragment>)
   }
 
 export default Brew;
