1a660cc3ad02e7467f8a279f93b671eb45f821cc
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-25 19:43:36 +0000

    Added code to update a specific brew from the database

--- bd-brewery-app/src/Components/SingleBrew.jsx
+++ bd-brewery-app/src/Components/SingleBrew.jsx
@@ -1,4 +1,5 @@
 import React, { Component, useEffect, useState } from 'react';
+import { Redirect } from 'react-router';
 import '../Stylesheets/Form.css';
 
 //set the url to send the data to
@@ -20,7 +21,7 @@ const divStyle = {
     // using react hooks to get data back - adapted from https://reactjs.org/docs/hooks-state.html
     const [brewId, setBrewId] = useState("");
     const [brew, setBrew] = useState("");
-    const [editBrew, setEditBrew] = useState("");
+    const [changeBrew, setChangeBrew] = useState(false); 
 
     const [brewNo, setBrewNo] = useState("");
     const [beer, setBeer] = useState("");
@@ -37,6 +38,7 @@ const divStyle = {
     const [bottleNo330, setBottleNo330] = useState("");
     const [duty, setDuty] = useState("");
     const [status, setStatus] = useState("");
+    const [routeRedirect, setRedirect] = useState(""); 
 
     const getBrew = () => {
         let id = props.match.params.id;
@@ -55,7 +57,6 @@ const divStyle = {
             console.log("response "+res.data);
             let parsed = JSON.parse(res.data);
             setBrew(parsed);
-
             setBrewNo(parsed.brewNo);
             setBeer(parsed.beer);
             setBatchNo(parsed.batchNo);
@@ -79,6 +80,120 @@ const divStyle = {
         getBrew();
     },[]);
 
+    const updateBrew = (e) => {
+        e.preventDefault();
+            //brew values to be sent to server
+            const brew = {
+                brewId: brewId,
+                brewNo: brewNo,
+                beer: beer,
+                batchNo: batchNo,
+                brewDate: brewDate,
+                og: og,
+                pg: pg,
+                abv: abv,
+                postConditionDate: postConditionDate,
+                postConditionVol: postConditionVol,
+                kegNo: kegNo,
+                bottleNo500: bottleNo500,
+                bottleNo330: bottleNo330,
+                duty: duty,
+                status: status
+            }
+
+          console.log(brew)
+          const options = { 
+            method: 'put',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+               body: JSON.stringify(brew)
+          
+          }
+          
+          fetch(url+"api/update/"+ brewId, options)
+          .then(res => {
+              return res.json();
+             
+          }).then(res => {
+              console.log(res)
+               setRedirect(true);
+          }).catch(err => {
+              console.log(err)
+          });
+    }
+
+    const redirect = routeRedirect;
+    if(redirect){
+         return <Redirect to="/brew" />  
+    }
+
+    const deleteItem = (brewId) => {
+        
+    }
+
+
+    const editItem = (brewId) => {
+        console.log(brewId)
+        setChangeBrew(!changeBrew);
+    }
+
+    let editForm;
+    if(changeBrew){
+        editForm =
+            <React.Fragment>
+                    <form onSubmit={updateBrew}>
+                    <div style={divStyle} className="float-left">
+                        <label>Brew No.</label>
+                        <input type="text" name="BrewNo" placeholder="Enter Brew Number" onChange= {event => setBrewNo(event.target.value)}  defaultValue={brew.brewNo}/>
+
+                        <label>Beer</label>
+                        <input type="text" name="beer" placeholder="Enter Beer" onChange={event => setBeer(event.target.value)} defaultValue={brew.beer}/>
+
+                        <label>Batch No</label>
+                        <input type="text" name="batchNo" placeholder="Enter Batch Number" onChange={event => setBatchNo(event.target.value)} defaultValue={brew.batchNo}/>
+
+                        <label>Brew Date</label>
+                        <input type="text" name="brewDate" placeholder="Enter Brew Date" onChange={event => setBrewDate(event.target.value)} defaultValue={brew.brewDate}/>
+
+                        <label>OG (Original Gravity)</label>
+                        <input type="text" name="og" placeholder="Enter OG" onChange={event => setOG(event.target.value)} defaultValue={brew.og}/>
+
+                        <label>PG (Present Gravity)</label>
+                        <input type="text" name="pg" placeholder="Enter PG" onChange={event => setPG(event.target.value)} defaultValue={brew.pg}/>
+
+                        <label>ABV</label>
+                        <input type="text" name="abv" placeholder="Enter ABV" onChange={event => setABV(event.target.value)} defaultValue={brew.abv}/>
+                    </div>
+                    <div className="float-right" style={divStyle}>
+                        <label>Post Conditioning Date</label>
+                        <input type="text" name="postConditionDate" placeholder="Enter Post Conditioning Date" onChange={event => setPCD(event.target.value)} defaultValue={brew.postConditionDate}/>
+
+                        <label>Post Conditioning Volume</label>
+                        <input type="text" name="postConditionVol" placeholder="Enter Post Conditioning Volume" onChange={event => setPCV(event.target.value)} defaultValue={brew.postConditionVol}/>
+
+                        <label>Keg No</label>
+                        <input type="text" name="kegNo" placeholder="Enter Keg Number" onChange={event => setKegNo(event.target.value)} defaultValue={brew.kegNo}/>
+
+                        <label>Bottle Number (500ml)</label>
+                        <input type="text" name="bottleNo500" placeholder="Enter Bottle Number (500ml)" onChange={event => setBottleNo500(event.target.value)} defaultValue={brew.bottleNo500}/>
+
+                        <label>Bottle Number (330ml)</label>
+                        <input type="text" name="bottleNo330" placeholder="Enter Bottle Number (330ml)" onChange={event => setBottleNo330(event.target.value)} defaultValue={brew.bottleNo330}/>
+
+                        <label>Duty</label>
+                        <input type="text" name="duty" placeholder="Enter Duty" onChange={event => setDuty(event.target.value)} defaultValue={brew.duty}/>
+
+                        <label>Status</label>
+                        <input type="text" name="status" placeholder="Enter Status" onChange={event => setStatus(event.target.value)} defaultValue={brew.status}/>
+                    </div>
+                    <input type="submit" value="Create Brew"/>
+                </form>
+
+                <button className="delete" onClick={(e) => deleteItem(brewId)}>Delete Item</button>
+            </React.Fragment>
+    }
+
     return(
         // React Fragment is a way of sending back multiple elements - https://reactjs.org/docs/fragments.html
         <React.Fragment> 
@@ -96,6 +211,9 @@ const divStyle = {
             <p>bottleNo330: {brew.bottleNo330}</p><br/>
             <p>duty: {brew.duty}</p><br/>
             <p>status: {brew.status}</p><br/>
+
+            <button className="edit" onClick={(e) => editItem(brewId)}>Edit Item</button>
+            {editForm}
             
         </React.Fragment>)
 }
