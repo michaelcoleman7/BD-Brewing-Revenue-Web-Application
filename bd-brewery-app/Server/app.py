ee4f11646508cb51835ead73d8572c26b57b5ef5
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-25 20:12:08 +0000

    Added delete blueprints to server

--- bd-brewery-app/Server/app.py
+++ bd-brewery-app/Server/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 # Routes
 from flask_cors import CORS
-from routes import indexRoute, createBrewRoute, brewRoute, updateBrewRoute
+from routes import indexRoute, createBrewRoute, brewRoute, updateBrewRoute, deleteBrewRoute
 app = Flask(__name__)
 CORS(app)
 
@@ -10,7 +10,8 @@ CORS(app)
 app.register_blueprint(indexRoute)
 app.register_blueprint(createBrewRoute)
 app.register_blueprint(brewRoute)
-app.register_blueprint(updateBrewRoute)
+app.register_blueprint(deleteBrewRoute)
+
 
 
 
