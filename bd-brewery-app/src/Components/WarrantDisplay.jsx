a995e600deaf722a981ad80ca9c1dcdd2e652e34
Michael Coleman <michaelcoleman135@gmail.com>
2020-02-27 23:57:39 +0000

    Fully setup Warrent Display

--- bd-brewery-app/src/Components/WarrantDisplay.jsx
+++ bd-brewery-app/src/Components/WarrantDisplay.jsx
@@ -1,83 +1,208 @@
 import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import { Redirect } from 'react-router';
-import Alert from 'react-bootstrap/Alert';
 import '../Stylesheets/Form.css';
-import DatePicker from 'react-date-picker';
-import { format } from 'date-fns';
 import ReactToPrint from "react-to-print";
 import Table from 'react-bootstrap/Table';
 
+//set the url to send the data to
+const url = "http://127.0.0.1:5000/"
 
 
   const WarrentDisplay = (props) => {
     const [repaymentsAllowed, setRepaymentsAllowed] = useState("");
-    const [repaymentsAllowedDuty, setRepaymentsAllowedDuty] = useState("");
     const [totalDutyOwed, setTotalDutyOwed] = useState("");
     const [totalHLPercent, setTotalHLPercent] = useState("");
     const [totalDutyOwedLessRepayents, setTotalDutyOwedLessRepayents] = useState("");
     const [lessRepaymentsDuty, setLessRepaymentsDuty] = useState("");
     const dutyPayable = 22.55;
-    
 
+    const [brewerName, setBrewerName] = useState("");
+    const [address, setAddress] = useState("");
+    const [warehouseName, setWarehouseName] = useState("");
+    const [IETWNo, setIETWNo] = useState("");
+    const [IEWKNo, setIEWKNo] = useState("");
+    const [payerRevenueNumber, setPayerRevenueNumber] = useState("");
+    const [taxType, setTaxType] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [designationofSignatory, setDesignationofSignatory] = useState("");
+    const [currentDate, setCurrentDate] = useState("");
+    const [currentMonth, setCurrentMonth] = useState("");
+    const [currentYear, setCurrentYear] = useState("");
+
+    // format date in required form, could also have used a react package to accomplish this - adpated from: https://stackoverflow.com/a/23593099
+    function formatCurrentDate() {
+        var d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+        return [day, month, year].join('-');
+    }
+
+    const getBreweryInfo = () => { 
+        fetch(url+"api/brewinfo").then(res => {
+            return res.json();
+        }).then(res => {
+            let parsed = JSON.parse(res.data);
+            setBrewerName(parsed.brewerName);
+            setAddress(parsed.address);
+            setWarehouseName(parsed.warehouseName);
+            setIETWNo(parsed.IETWNo);
+            setIEWKNo(parsed.IEWKNo);
+            setPayerRevenueNumber(parsed.payerRevenueNumber);
+            setTaxType(parsed.taxType);
+            setPhoneNumber(parsed.phoneNumber);
+            setDesignationofSignatory(parsed.designationofSignatory);
+            setCurrentDate(formatCurrentDate());
+            //set months array with month names as getMonth returns integer value of month
+            let monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+            setCurrentMonth(monthNames[new Date().getMonth()]);
+            setCurrentYear(new Date().getFullYear());
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        getBreweryInfo();
+    },[]);
+    
+    //calcvul;ate values needed based on repayment values sent via props
     const setupCalculations = () => {
         setRepaymentsAllowed(props.location.state.repaymentsAllowed);
         setTotalHLPercent(props.location.state.totalHLPercent);
         setTotalDutyOwed(props.location.state.totalDutyOwed);
-        setTotalDutyOwedLessRepayents(parseFloat(props.location.state.totalDutyOwed) - parseFloat(props.location.state.repaymentsAllowed).toFixed(2));
         setLessRepaymentsDuty(parseFloat(props.location.state.repaymentsAllowed * dutyPayable/2).toFixed(2));
-        
+        setTotalDutyOwedLessRepayents(parseFloat(props.location.state.totalDutyOwed) - parseFloat(props.location.state.repaymentsAllowed * dutyPayable/2).toFixed(2));    
     }
 
     useEffect(() => {
         setupCalculations();
     },[]);
+
+    //style divs for paragraphs
+    const pStyle = {
+        float: 'left'
+      };
+      const pStyle2 = {
+        float: 'right',
+        display : 'inline-block'
+      };
     
     //adapted from - https://www.npmjs.com/package/react-to-print
     class BrewInformation extends React.Component {
         render() {
           return (
             <center>
-            <Card style={{ width: '50%' }}>
+            <Card style={{ width: '80%' }}>
                 <Card.Body>
                     <Card.Title><h3><b>BREWER'S BEER DUTY RETURN</b></h3></Card.Title>
                     <Card.Text>
-                        <Table striped bordered hover>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th colSpan="2">Name and Address of Person Paying Duty:</th>
+                                <th colSpan="2">No. and Date (For Official Use):</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td colSpan="2" rowSpan="2">{address}</td>
+                                <td colSpan="2"><b>Warehouse Name: </b><br/>{warehouseName}</td>
+                                <td><b>IETW No:</b><br/>{IETWNo}</td>
+                                <td><b>IEWK No:</b><br/>{IEWKNo}</td>
+                            </tr>
+                            <tr>
+                                <td><b>Month:</b><br/>{currentMonth}<br/></td>
+                                <td><b>Year:</b><br/>{currentYear}</td>
+                                <td><b>Payer Revenue Number:</b><br/>{payerRevenueNumber}</td>
+                                <td><b>Tax Type:</b><br/>{taxType}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    <br/><br/>
+                    <Table striped bordered hover>
                         <thead>
                             <tr>
                                 <th>Excise Home (ERN 9820)</th>
-                                <th>Quantity (HL%)</th>
-                                <th>Rate of Duty</th>
-                                <th>Duty Payable (€)</th>
+                                <th>Quantity (HL%):</th>
+                                <th>Rate of Duty:</th>
+                                <th>Duty Payable (€):</th>
+                                </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>Delivered for Home Consumption:</th>
+                                <td>{totalHLPercent}</td>
+                                <td>{dutyPayable}/2</td>
+                                <td>{totalDutyOwed}</td>
+                            </tr>
+                            <tr>
+                                <th>Less Repayment amounts:</th>
+                                <td>{repaymentsAllowed}</td>
+                                <td>{dutyPayable}/2</td>
+                                <td>{lessRepaymentsDuty}</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <th>Net Payable:</th>
+                                <td></td>
+                                <td colSpan="2">{totalDutyOwedLessRepayents}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                        <br/><br/>
+                        <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Value for VAT:</th>
+                                <th>Rate of VAT:</th>
+                                <th>VAT Payable (€):</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th>Delivered for Home Consumption:</th>
-                                    <td>{totalHLPercent}</td>
-                                    <td>{dutyPayable}/2</td>
-                                    <td>{totalDutyOwed}</td>
-                                </tr>
-                                <tr>
-                                    <th>Less Repayment amounts</th>
-                                    <td>{repaymentsAllowed}</td>
-                                    <td>{dutyPayable}/2</td>
-                                    <td>{lessRepaymentsDuty}</td>
-                                </tr>
-                                <tr>
                                     <td></td>
-                                    <th>Net Payable</th>
-                                    <td>{dutyPayable}/2</td>
-                                    <td colSpan="2">{totalDutyOwedLessRepayents}</td>
+                                    <td></td>
+                                    <td></td>
                                 </tr>
                             </tbody>
                         </Table>
+                        <br/><br/>
+                        <Table striped bordered hover>
+                            <tbody>
+                                <tr>
+                                    <th>Total Amount for Direct Debit:</th>
+                                    <td>{totalDutyOwedLessRepayents}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Card.Text>
+                    <br/>
+                    <Card.Text>
+                        <p>I <b>{brewerName}</b> declare that the particulars herin and on the attached schedules are true and complete and that the above amount will be paid by Direct Debit under the Revenue Number shown herein</p>
+                        <p style={pStyle}><b>Signature:</b> _______________________________________</p><br/> <p style={pStyle2}><b>Date: </b>{currentDate}</p><br/>
+                        <p style={pStyle}><b>Designation of Signatory:</b> {designationofSignatory}</p><br/> <p style={pStyle2}><b>Phone Number:</b> {phoneNumber}</p><br/><br/>
+
+                        <p>(1) The quantity of beer to be entered here is the quantity delivered from warehouse for home consumption, 
+                            including any beer consumed on the brewery premises, plus any additions for previous underdeclarations.</p>
+                            <p>(2) Only repayments that have been authorised by the Revenue Official can be deducted here.</p>
+
                     </Card.Text>
                 </Card.Body>
             </Card></center>
           );
         }
-      }    
+      } 
+      
+      
     class BrewDisplay extends React.Component {
         render() {
         return (
