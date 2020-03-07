636a0e7634cee3d582c4c4d0ece19cfa93ab9b42
Michael Coleman <michaelcoleman135@gmail.com>
2020-03-07 18:53:24 +0000

    Added sorting of stock returns by date

--- bd-brewery-app/src/Components/StockReturnList.jsx
+++ bd-brewery-app/src/Components/StockReturnList.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroup from 'react-bootstrap/ListGroup';
+import DatePicker from 'react-date-picker';
+import { format } from 'date-fns';
 
 //set the url to receive the data from
 const url = "http://127.0.0.1:5000/"
@@ -8,6 +10,8 @@ const url = "http://127.0.0.1:5000/"
 const StockReturnList = () => {
 
   const [stockReturns, setStockReturns] = useState([]);
+  const [monthDate, setMonthDate] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("None Selected");
   const getStockReturns = () => {
     fetch(url+"api/stockreturn").then(res =>{
       return res.json();
@@ -35,11 +39,30 @@ const StockReturnList = () => {
     background: 'rgba(144, 84, 23, 0.5)'
   };
 
+  let stockReturnList = []
+  for (var i = 0; i < stockReturns.length; i++) {
+    console.log(stockReturns[i].stockReturnDate);
+    console.log(monthDate);
+    if(stockReturns[i].stockReturnDate == monthDate){
+      stockReturnList.push(stockReturns[i]);
+    }
+    else if(monthDate == ""){
+      stockReturnList.push(stockReturns[i]);
+    }
+  }
+
+  let newDate;
+  const dateChange = (date) => {
+          newDate = format(new Date(date), 'MM-yyyy')
+          setMonthDate(newDate);
+          setSelectedMonth("Current selected Month is: "+ newDate);
+  }
+
 
   let stockReturnArray;
-  if(stockReturns.length > 0){
+  if(stockReturnList.length > 0){
     stockReturnArray = <div>
-        {stockReturns.map(stockReturn => {
+        {stockReturnList.map(stockReturn => {
           return(
             <div key={stockReturn._id}>
               <Link to={"../stockreturn/"+stockReturn._id}>
@@ -63,6 +86,8 @@ const StockReturnList = () => {
 return(
     <React.Fragment> 
       <div>
+      <h5 style={{color: "white"}}>Select a Date in the month you wish to sort by: {selectedMonth}</h5>
+      <DatePicker format="MM/yyyy" onChange={event => dateChange(event)}/>
       <h1 style={{color: "white"}}>Stock Return List</h1>
         {stockReturnArray}
       </div>
